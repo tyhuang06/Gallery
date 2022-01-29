18e11f591950cf54415a7c1edf5f6294e1df21f7
Jamie Huang <tyhuang06@gmail.com>
2022-01-29 08:37:41 -0500

    Implemented the gorilla mux router

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my site</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/contact", contactHandler)
+
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
 }
