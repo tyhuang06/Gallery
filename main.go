bd2f68866cd8672836333ed290afafc0ff7ea775
Jamie Huang <tyhuang06@gmail.com>
2022-02-20 19:37:41 -0500

    Created controller for static pages

--- main.go
+++ main.go
@@ -6,43 +6,19 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/tyhuang06/Gallery/controllers"
-	"github.com/tyhuang06/Gallery/views"
 )
-
-var (
-	homeView    *views.View
-	contactView *views.View
-)
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	must(homeView.Render(w, nil))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	must(contactView.Render(w, nil))
-}
 
 func main() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", homeHandler).Methods("GET")
-	r.HandleFunc("/contact", contactHandler).Methods("GET")
+	r.Handle("/", staticC.Home).Methods("GET")
+	r.Handle("/contact", staticC.Contact).Methods("GET")
 	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
-}
-
-func must(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
--- views/view.go
+++ views/view.go
@@ -30,8 +30,15 @@ type View struct {
 	Layout   string
 }
 
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
 // Render view with predefined layout
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
 
